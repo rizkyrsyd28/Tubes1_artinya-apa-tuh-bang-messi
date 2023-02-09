fc2b733af3b93c2e47c6b0610538a718ad1141b7
Myumet <16521017@mahasiswa.itb.ac.id>
2023-02-09 14:56:56 +0700

    fisrt setup

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,15 @@ import Enums.*;
 import Models.*;
 import Services.*;
 import com.microsoft.signalr.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
 
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Logger logger = LoggerFactory.getLogger(Main.class);
+        // Logger logger = LoggerFactory.getLogger(Main.class);
         BotService botService = new BotService();
         String token = System.getenv("Token");
         token = (token != null) ? token : UUID.randomUUID().toString();
@@ -35,7 +35,7 @@ public class Main {
             System.out.println("Registered with the runner " + id);
 
             Position position = new Position();
-            GameObject bot = new GameObject(id, 10, 20, 0, position, ObjectTypes.PLAYER);
+            GameObject bot = new GameObject(id, 10, 20, 0, position, ObjectTypes.PLAYER, 0, 0, 0, 0, 0);
             botService.setBot(bot);
         }, UUID.class);
 
--- src/main/java/Models/GameObject.java
+++ src/main/java/Models/GameObject.java
@@ -10,14 +10,26 @@ public class GameObject {
   public Integer currentHeading;
   public Position position;
   public ObjectTypes gameObjectType;
+  public Integer effects;
+  public Integer torpedoCount;
+  public Integer supernovaAvailable;
+  public Integer teleCount;
+  public Integer shieldCount;
 
-  public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position, ObjectTypes gameObjectType) {
+  public GameObject(UUID id, Integer size, Integer speed, Integer currentHeading, Position position,
+      ObjectTypes gameObjectType, Integer effects, Integer torpedoCount, Integer supernovaAvailable,
+      Integer teleCount, Integer shieldCount) {
     this.id = id;
     this.size = size;
     this.speed = speed;
     this.currentHeading = currentHeading;
     this.position = position;
     this.gameObjectType = gameObjectType;
+    this.effects = effects;
+    this.torpedoCount = torpedoCount;
+    this.supernovaAvailable = supernovaAvailable;
+    this.teleCount = teleCount;
+    this.shieldCount = shieldCount;
   }
 
   public UUID getId() {
@@ -60,9 +72,65 @@ public class GameObject {
     this.gameObjectType = gameObjectType;
   }
 
-  public static GameObject FromStateList(UUID id, List<Integer> stateList)
-  {
-    Position position = new Position(stateList.get(4), stateList.get(5));
-    return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position, ObjectTypes.valueOf(stateList.get(3)));
+  public Integer getCurrentHeading() {
+    return currentHeading;
   }
+
+  public void setCurrentHeading(Integer currentHeading) {
+    this.currentHeading = currentHeading;
+  }
+
+  public Integer getEffects() {
+    return effects;
+  }
+
+  public void setEffects(Integer effects) {
+    this.effects = effects;
+  }
+
+  public Integer getTorpedoCount() {
+    return torpedoCount;
+  }
+
+  public void setTorpedoCount(Integer torpedoCount) {
+    this.torpedoCount = torpedoCount;
+  }
+
+  public Integer getSupernovaAvailable() {
+    return supernovaAvailable;
+  }
+
+  public void setSupernovaAvailable(Integer supernovaAvailable) {
+    this.supernovaAvailable = supernovaAvailable;
+  }
+
+  public Integer getTeleCount() {
+    return teleCount;
+  }
+
+  public void setTeleCount(Integer teleCount) {
+    this.teleCount = teleCount;
+  }
+
+  public Integer getShieldCount() {
+    return shieldCount;
+  }
+
+  public void setShieldCount(Integer shieldCount) {
+    this.shieldCount = shieldCount;
+  }
+
+  public static GameObject FromStateList(UUID id, List<Integer> stateList) {
+    Position position = new Position(stateList.get(4), stateList.get(5));
+    if (stateList.get(3) == 1) {
+      return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position,
+          ObjectTypes.valueOf(stateList.get(3)), stateList.get(6), stateList.get(7),
+          stateList.get(8), stateList.get(9), stateList.get(10));
+    } else {
+      return new GameObject(id, stateList.get(0), stateList.get(1), stateList.get(2), position,
+          ObjectTypes.valueOf(stateList.get(3)), 0, 0, 0, 0,
+          0);
+    }
+  }
+
 }
